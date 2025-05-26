1b2fb1405ad6e8199a761c31aa09fdb5e295de5a
Talal Ilyas <talalilyas11@gmail.com>
2025-05-26 22:08:30 +0500

    Add/ new route on the student login

--- Pagerouting.js
+++ Pagerouting.js
@@ -1,27 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Login from './Login';
-import { Image } from 'antd';
-// import StepTwo from './StepTwo';
-// import StepThree from './StepThree';
+import Studentlogin from './Studentlogin';
 
 function Pagerouting() {
-
-
-
-
-
   return (
-
-
-
-    <Routes >
-     { <Route path="/" element={<Login />} />
-    
-    
-  } 
-      {/* <Route path="/step-2" element={<StepTwo />} />
-      <Route path="/step-3" element={<StepThree />} /> */}
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/studentlogin" element={<Studentlogin />} />
     </Routes>
   );
 }
