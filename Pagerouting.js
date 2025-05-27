58cf238048f38ac2a132dea8ee2b30fe0394b8bf
Talal Ilyas <talalilyas11@gmail.com>
2025-05-28 00:09:44 +0500

    Route/to the lecturer

--- Pagerouting.js
+++ Pagerouting.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './Login';
 import Studentlogin from './Studentlogin';
+import Lecturerlogin from './Lecturerlogin';
+import Studentdashboard from './Studentdashboard';
+import useLocalStorageState from 'use-local-storage-state';
 
 function Pagerouting() {
+  const [isLogin] = useLocalStorageState("isLogin", false);
+
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/studentlogin" element={<Studentlogin />} />
+      <Route path="/Lecturerlogin" element={<Lecturerlogin />} />
+      
+      {isLogin 
+        ? <Route path="/Studentdashboard" element={<Studentdashboard />} />
+        : <Route path="/Studentdashboard" element={<Navigate to="/" />} />
+      }
+
+   
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
 }
