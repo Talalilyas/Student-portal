2f111953194d242e9a2e5570572028afc254b48f
Talal Ilyas <talalilyas11@gmail.com>
2025-05-29 01:38:19 +0500

    make the route for it

--- Pagerouting.js
+++ Pagerouting.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './Login';
 import Studentlogin from './Studentlogin';
-import Lecturerlogin from './Lecturerlogin';  
+import Lecturerlogin from './Lecturerlogin';
 import Studentdashboard from './Studentdashboard';
+import Coursedatatable from './Coursedatatable'; // Assuming this is your table page
 import useLocalStorageState from 'use-local-storage-state';
 
 function Pagerouting() {
@@ -11,16 +12,22 @@ function Pagerouting() {
 
   return (
     <Routes>
+      {/* Public Routes */}
       <Route path="/" element={<Login />} />
       <Route path="/studentlogin" element={<Studentlogin />} />
-      <Route path="/Lecturerlogin" element={<Lecturerlogin />} />
-      
-      {isLogin 
-        ? <Route path="/Studentdashboard" element={<Studentdashboard />} />
-        : <Route path="/Studentdashboard" element={<Navigate to="/" />} />
-      }
+      <Route path="/lecturerlogin" element={<Lecturerlogin />} />
 
-   
+      {/* Protected Dashboard Route */}
+      {isLogin ? (
+        <Route path="/Studentdashboard" element={<Studentdashboard />}>
+          <Route index element={<h2>Welcome to the Dashboard</h2>} />
+          <Route path="table" element={<Coursedatatable />} />
+        </Route>
+      ) : (
+        <Route path="/Studentdashboard/*" element={<Navigate to="/" />} />
+      )}
+
+      {/* Catch-all */}
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
