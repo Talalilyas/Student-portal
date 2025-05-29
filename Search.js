8c2827454759c639c6d6f27ea763a70017c0ffd8
Talal Ilyas <talalilyas11@gmail.com>
2025-05-30 02:17:32 +0500

    Add-serach-bar-header

---
+++ Search.js
@@ -0,0 +1,20 @@
+// Search.jsx
+import React from "react";
+import { Input } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
+
+export default function Search({ onSearch }) {
+  return (
+    <Input
+      placeholder="What are you looking for?"
+      onPressEnter={onSearch} 
+      suffix={<SearchOutlined style={{ color: "#aaa" }} />}
+      style={{
+        width: "100%",
+        maxWidth: 600,
+        marginTop: "2px",
+        marginLeft: "15px",
+      }}
+    />
+  );
+}
