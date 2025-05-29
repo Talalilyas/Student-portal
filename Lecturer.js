e76f468aec86da9adee7c5f7baa88a146ac19d52
Talal Ilyas <talalilyas11@gmail.com>
2025-05-29 15:18:00 +0500

    Improvements

--- Lecturer.js
+++ Lecturer.js
@@ -1,20 +1,9 @@
 import React from "react";
 import { useState } from "react";
 
-import {
-  Button,
-  Input,
-  Form,
-  Row,
-  Col,
-  Image,
-  Typography,
-  Radio,
-  Space,
-  Tooltip,
-} from "antd";
+import { Row, Col, Radio } from "antd";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 export default function Lecturer() {
   const [role, setRole] = useState("student");
 
@@ -23,46 +12,46 @@ export default function Lecturer() {
   };
 
   return (
-    <Row   justify="center">
-    <Col xs="24"  justify="center">
-      <Radio.Group
-        onChange={handleChange}
-        value={role}
-        buttonStyle="solid"
-        style={{
-          border: "1px solid  black",
-          borderRadius: "999px",
-          overflow: "hidden",
-        }}
-      >  
-        <Link   to="./studentlogin" >
-        <Radio.Button
-          value="student"
+    <Row justify="center">
+      <Col xs="24" justify="center">
+        <Radio.Group
+          onChange={handleChange}
+          value={role}
+          buttonStyle="solid"
           style={{
-            borderRadius: "999px 0 0 999px",
-            width: 120,
-            textAlign: "center",
+            border: "1px solid  black",
+            borderRadius: "999px",
+            overflow: "hidden",
           }}
         >
-          Student
-        </Radio.Button>
-        </Link>
-        <Link to="./Lecturerlogin"> 
-        <Radio.Button
-          value="lecturer"
-          style={{
-            borderRadius: "0 999px 999px 0",
+          <Link to="./studentlogin">
+            <Radio.Button
+              value="student"
+              style={{
+                borderRadius: "999px 0 0 999px",
+                width: 120,
+                textAlign: "center",
+              }}
+            >
+              Student
+            </Radio.Button>
+          </Link>
+          <Link to="./Lecturerlogin">
+            <Radio.Button
+              value="lecturer"
+              style={{
+                borderRadius: "0 999px 999px 0",
 
-            maxWidth: "100%",
+                maxWidth: "100%",
 
-            textAlign: "center",
-          }}
-        >
-          Lecturer
-        </Radio.Button>
-        </Link>
-      </Radio.Group>
-    </Col>
+                textAlign: "center",
+              }}
+            >
+              Lecturer
+            </Radio.Button>
+          </Link>
+        </Radio.Group>
+      </Col>
     </Row>
   );
 }
