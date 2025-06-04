d79abe34dcde95397112d4d2e0852920311e9fbb
Talal Ilyas <talalilyas11@gmail.com>
2025-06-05 01:51:20 +0500

    Improvements-Remove-conflicts

--- Pagerouting.js
+++ Pagerouting.js
@@ -1,36 +1,44 @@
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, Outlet } from 'react-router-dom';
 import Login from './Login';
 import Studentlogin from './Studentlogin';
 import Lecturerlogin from './Lecturerlogin';
-import Studentdashboard from './Studentdashboard';
-import Coursedatatable from './Coursedatatable'; // Assuming this is your table page
+import Studentdashboard from './Studentdashboard'; // acts as layout
+import Coursedatatable from './Coursedatatable';
+import Studentexam from './Studentexam';
 import useLocalStorageState from 'use-local-storage-state';
 
+// Layout for dashboard that renders nested routes
+const DashboardLayout = () => (
+  <div className="dashboard-layout">
+    <Studentdashboard />
+    <Outlet />
+  </div>
+);
 
 function Pagerouting() {
-  const [isLogin] = useLocalStorageState("isLogin", false);
+  const [isLogin] = useLocalStorageState('isLogin', false);
 
   return (
     <Routes>
-
+      {/* Public routes */}
       <Route path="/" element={<Login />} />
       <Route path="/studentlogin" element={<Studentlogin />} />
       <Route path="/lecturerlogin" element={<Lecturerlogin />} />
 
-     
-    {isLogin ? (
-  <Route path="/Studentdashboard" element={<Studentdashboard />}>
-    <Route index element={<h2>Welcome to the Dashboard</h2>} />
-    <Route path="mycourse" element={<Coursedatatable />} />
- 
-  </Route>
-) : (
-  <Route path="/Studentdashboard/*" element={<Navigate to="/" />} />
-)}
+      {/* Protected routes */}
+      {isLogin ? (
+        <Route path="/studentdashboard" element={<DashboardLayout />}>
+          <Route index element={<h2>Welcome to the Dashboard</h2>} />
+          <Route path="mycourse" element={<Coursedatatable />} />
+          <Route path="coursecatalogue" element={<Studentexam />} />
+        </Route>
+      ) : (
+        <Route path="/studentdashboard/*" element={<Navigate to="/" replace />} />
+      )}
 
       {/* Catch-all */}
-      <Route path="*" element={<Navigate to="/" />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
--- Sidebar.js
+++ Sidebar.js
@@ -15,13 +15,14 @@ import {
 import { Menu, Space, Avatar, Divider, Layout, Typography } from "antd";
 import { useNavigate } from "react-router-dom";
 import image from "./images.jpg";
+import { useState } from "react";
 
 const { Sider } = Layout;
 const { Text } = Typography;
 
-export default function Sidebar({ collapsed, setCollapsed, handleSignOut }) {
+export default function Sidebar({  handleSignOut }) {
   const navigate = useNavigate();
-
+  const [collapsed, setCollapsed] = useState(false);
   return (
     <Sider
       width={260}
@@ -33,9 +34,17 @@ export default function Sidebar({ collapsed, setCollapsed, handleSignOut }) {
       style={{ background: "white" }}
     >
       <Space align="start" style={{ marginLeft: "8px", marginTop: "30px" }}>
-        <Avatar shape="circle" src={image} style={{ width: "60px", height: "60px" }} />
+        <Avatar
+          shape="circle"
+          src={image}
+          style={{ width: "60px", height: "60px" }}
+        />
         {!collapsed && (
-          <Space direction="vertical" size={0} style={{ marginTop: "10px", marginLeft: "19px" }}>
+          <Space
+            direction="vertical"
+            size={0}
+            style={{ marginTop: "10px", marginLeft: "19px" }}
+          >
             <Text strong>Student 1</Text>
             <Space>
               <Text>Status:</Text>
@@ -48,34 +57,79 @@ export default function Sidebar({ collapsed, setCollapsed, handleSignOut }) {
       <Divider size="medium" />
 
       <Menu mode="inline" theme="light">
-        <Menu.Item key="courses" icon={<UnorderedListOutlined />} onClick={() => navigate("mycourse")} style={{marginTop:"15px"}}>
+        <Menu.Item
+          key="courses"
+          icon={<UnorderedListOutlined />}
+          onClick={() => navigate("mycourse")}
+          style={{ marginTop: "15px" }}
+        >
           My Courses
         </Menu.Item>
-        <Menu.Item key="grade" icon={<ReadOutlined />} onClick={() => navigate("grade")}  style={{marginTop:"15px"}}>
+        <Menu.Item
+          key="grade"
+          icon={<ReadOutlined />}
+          onClick={() => navigate("coursecatalogue")}
+          style={{ marginTop: "15px" }}
+        >
           Course Catalogue
         </Menu.Item>
-        <Menu.Item key="settings" icon={<AppstoreOutlined />} onClick={() => navigate("settings")}  style={{marginTop:"15px"}}>
+        <Menu.Item
+          key="settings"
+          icon={<AppstoreOutlined />}
+          onClick={() => navigate("settings")}
+          style={{ marginTop: "15px" }}
+        >
           My Progress
         </Menu.Item>
-        <Menu.Item key="calendar" icon={<CalendarOutlined />} onClick={() => navigate("quotes")}  style={{marginTop:"15px"}}>
+        <Menu.Item
+          key="calendar"
+          icon={<CalendarOutlined />}
+          onClick={() => navigate("quotes")}
+          style={{ marginTop: "15px" }}
+        >
           Academic Calendar
         </Menu.Item>
-        <Menu.Item key="result-form" icon={<ScheduleOutlined />} onClick={() => navigate("result-form")}  style={{marginTop:"15px"}}>
+        <Menu.Item
+          key="result-form"
+          icon={<ScheduleOutlined />}
+          onClick={() => navigate("result-form")}
+          style={{ marginTop: "15px" }}
+        >
           My Results
         </Menu.Item>
 
         <Divider size="medium" />
 
-        <Menu.Item key="study-card" icon={<UserOutlined />} onClick={handleSignOut}  style={{marginTop:"15px"}}>
+        <Menu.Item
+          key="study-card"
+          icon={<UserOutlined />}
+          onClick={handleSignOut}
+          style={{ marginTop: "15px" }}
+        >
           Study Card
         </Menu.Item>
-        <Menu.Item key="recommendations" icon={<LikeOutlined />} onClick={handleSignOut}  style={{marginTop:"15px"}}>
+        <Menu.Item
+          key="recommendations"
+          icon={<LikeOutlined />}
+          onClick={handleSignOut}
+          style={{ marginTop: "15px" }}
+        >
           Recommendations
         </Menu.Item>
-        <Menu.Item key="rating" icon={<StarOutlined />} onClick={handleSignOut}  style={{marginTop:"15px"}}>
+        <Menu.Item
+          key="rating"
+          icon={<StarOutlined />}
+          onClick={handleSignOut}
+          style={{ marginTop: "15px" }}
+        >
           My Rating
         </Menu.Item>
-        <Menu.Item key="signout" icon={<LogoutOutlined />} style={{ color: "red" ,marginTop:"15px"}} onClick={handleSignOut} >
+        <Menu.Item
+          key="signout"
+          icon={<LogoutOutlined />}
+          style={{ color: "red", marginTop: "15px" }}
+          onClick={handleSignOut}
+        >
           Sign Out
         </Menu.Item>
       </Menu>
