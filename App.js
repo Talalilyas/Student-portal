c2eb1ff26023e8815a67db80641088fc22837f49
Talal Ilyas <talalilyas11@gmail.com>
2025-06-23 15:26:26 +0500

    little tune up and clear extra file

--- App.js
+++ App.js
@@ -1,25 +1,14 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Login from './Login';
-import { Image } from 'antd';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import { Image } from "antd";
 // import StepTwo from './StepTwo';
 // import StepThree from './StepThree';
 
 function App() {
-
-
-
-
-
   return (
-
-
-
-    <Routes >
-     { <Route path="/" element={<Login />} />
-    
-    
-  } 
+    <Routes>
+      {<Route path="/" element={<Login />} />}
       {/* <Route path="/step-2" element={<StepTwo />} />
       <Route path="/step-3" element={<StepThree />} /> */}
     </Routes>
