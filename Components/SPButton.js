b50cc4d307d99fa1cfe39d7fb1b02c5ffb559286
Talal Ilyas <talalilyas11@gmail.com>
2025-06-24 15:47:16 +0500

    improvements-reuseble-components

---
+++ Components/SPButton.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Form, Button } from "antd";
+import SPInput from "./SPinput";
+
+const SPButton = () => {
+  return (
+    <Form.Item>
+      <Button type="primary" htmlType="submit" block>
+        Login
+      </Button>
+    </Form.Item>
+  );
+};
+
+export default SPButton;
