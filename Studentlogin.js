3f5fa27a6ea8f2fd89e263f24630988b47a08198
Talal Ilyas <talalilyas11@gmail.com>
2025-06-24 15:47:34 +0500

    tuneup/components

--- Studentlogin.js
+++ Studentlogin.js
@@ -1,11 +1,18 @@
-import { Row, Col, Image, Typography, message } from "antd";
-import bgimage from "./Images/university-background-image.jpg";
-import useLocalStorageState from "use-local-storage-state";
-import { Form, Input, Button, Card } from "antd";
+import { Row, Col, Typography, message } from "antd";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import useLocalStorageState from "use-local-storage-state";
 
-export default function Studentlogin() {
+import { Form, Input, Card, Button } from "antd";
+import SPInput from "./Components/SPinput";
+
+import SPFormInput from "./Components/SPFrominput";
+import SpTitle from "./Components/SpTitle";
+import SPButton from "./Components/SPButton";
+import SPlogoheader from "./Components/SPlogoheader";
+import SPTitle from "./Components/SpTitle";
+
+export default function StudentLogin() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [isLogin, setIsLogin] = useLocalStorageState("isLogin", false);
@@ -14,107 +21,70 @@ export default function Studentlogin() {
 
   const navigate = useNavigate();
 
-
-    console.log(isLogin);
-    console.log(accessToken)
   const handleSubmit = async () => {
     const loginData = {
-      username: username,
-      password: password,
+      username,
+      password,
       expiresInMins: 30,
     };
-
     try {
       const response = await fetch("https://dummyjson.com/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(loginData),
       });
-
       if (!response.ok) {
         const errorDetails = await response.json();
         throw new Error(errorDetails.message || "Invalid credentials");
       }
-
       const data = await response.json();
       message.success("Login successful!");
       setAccessToken(data.token);
       setUser({ username });
       setIsLogin(true);
-      console.log(users)
       navigate("/studentdashboard");
     } catch (err) {
-      message.error(`Error: ${err.message}`);
+      message.error(`Login failed: ${err.message}`);
     }
   };
 
-  const { Title } = Typography;
-
   return (
     <>
-      <Row justify="center" style={{ width: "100%" }}>
-        <Col>
-          <Image
-            src={bgimage}
-            preview={false}
-            style={{ width: "150px", height: "auto" }}
-          />
-        </Col>
-      </Row>
-
+      <SPlogoheader />
+      <SPTitle text={"Student perfoming portal login"} />
       <Row justify="center">
-        <Col>
-          <Title style={{ fontFamily: "cursive", textAlign: "center" }}>
-            Student Performing Portal Login
-          </Title>
-        </Col>
-      </Row>
-
-      <Row justify="center" align="middle">
-        <Card
-          style={{
-            backgroundColor: "rgba(0, 0, 0, 0.7)",
-            color: "#fff",
-            width: "100%",
-            maxWidth: 350,
-          }}
-        >
-          <Form name="loginForm" layout="vertical" onFinish={handleSubmit}>
-            <Form.Item
-              label={<span style={{ color: "#fff" }}>Student ID</span>}
-              name="username"
-              rules={[
-                { required: true, message: "Please enter your username!" },
-              ]}
-            >
-              <Input
+        <Col span={6}>
+          <Card
+            style={{
+              backgroundColor: "rgba(0, 0, 0, 0.7)",
+            }}
+          >
+            <Form name="loginForm" layout="vertical" onFinish={handleSubmit}>
+              <SPFormInput
+                label="Student ID"
+                name="username"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
                 placeholder="Enter Your ID"
+                rules={[
+                  { required: true, message: "Please enter your username!" },
+                ]}
               />
-            </Form.Item>
-
-            <Form.Item
-              label={<span style={{ color: "#fff" }}>Password</span>}
-              name="password"
-              rules={[
-                { required: true, message: "Please enter your password!" },
-              ]}
-            >
-              <Input.Password
+              <SPFormInput
+                label="Password"
+                name="password"
+                type="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 placeholder="Enter your password"
+                rules={[
+                  { required: true, message: "Please enter your password!" },
+                ]}
               />
-            </Form.Item>
-
-            <Form.Item>
-              <Button type="primary" htmlType="submit" block>
-                Login
-              </Button>
-            </Form.Item>
-          </Form>
-        </Card>
+              <SPButton />
+            </Form>
+          </Card>
+        </Col>
       </Row>
     </>
   );
