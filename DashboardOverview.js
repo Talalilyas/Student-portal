50f0201b9ff3c1d526f5fa6cfc779f053dedf854
Talal Ilyas <talalilyas11@gmail.com>
2025-07-14 17:15:38 +0500

    Added the Anoucment:data come from the api

--- DashboardOverview.js
+++ DashboardOverview.js
@@ -1,61 +1,96 @@
 import React from "react";
-import { Row, Col, Typography, Card } from "antd";
+import { Row, Col, Typography, Table } from "antd";
 import { UnorderedListOutlined } from "@ant-design/icons";
 
 import DashboardCardSection from "./Components/DashboardCardSection";
 import CourseTable from "./CourseTable";
 import AcademicCalendar from "./Components/SPcalender";
 import Studentexam from "./Studentexam";
+import useFetch from "./FetchHOOk/Hookfetchdata";
+import { SPtable } from "./Components";
 
-const { Text} = Typography;
-
-const announcements = [
-  "Midterm exams start next week.",
-  "New course for Fall 2025.",
-  "Graduation form deadline: June 25.",
-];
+const { Text } = Typography;
 
 export default function DashboardOverview() {
+  const {
+    data: announcements,
+    loading,
+    error,
+  } = useFetch("http://localhost:8080/SPAnoucments");
+console.log("Announcements data:", announcements);
+
+
+  const columns = [
+    {
+      title: "ID",
+      dataIndex: "key",
+      key: "key",
+      width: 60,
+    },
+    {
+      title: "Announcement",
+      dataIndex: "announcement_text",
+      key: "announcement_text",
+    },
+  ];
+
+
+  const formattedData = announcements?.map((item, index) => ({
+    ...item,
+    key: index + 1,
+  }));
+
   return (
     <>
       <Row gutter={[16, 16]}>
         <Col xs={24} lg={17}>
           <DashboardCardSection
             icon={<UnorderedListOutlined />}
-            title="My Courses">
+            title="My Courses"
+          >
             <CourseTable />
           </DashboardCardSection>
         </Col>
+
         <Col xs={24} lg={7}>
           <DashboardCardSection
             icon={<UnorderedListOutlined />}
             title="Academic Calendar"
-            style={{ marginLeft: 4 }}>
+          >
             <AcademicCalendar />
           </DashboardCardSection>
         </Col>
       </Row>
+
       <Row gutter={[16, 16]}>
         <Col xs={24} lg={17}>
           <DashboardCardSection
             icon={<UnorderedListOutlined />}
-            title="Upcoming Exams" >
+            title="Upcoming Exams"
+          >
             <Studentexam />
           </DashboardCardSection>
         </Col>
-        <Col xs={24} lg={7}>
+
+        <Col xs={24} lg={7} hi >
           <DashboardCardSection
             icon={<UnorderedListOutlined />}
-            title="Announcements">
-            <Row gutter={[8, 8]}>
-              {announcements.map((item, index) => (
-                <Col span={24} key={index}>
-                  <Card bordered={false} style={{ backgroundColor: "#f9f9f9" }}>
-                    <Text>{item}</Text>
-                  </Card>
-                </Col>
-              ))}
-            </Row>
+            title="Announcements"
+          >
+            {loading ? (
+              <p>Loading announcements...</p>
+            ) : error ? (
+              <p style={{ color: "red" }}>Error: {error}</p>
+            ) : (
+              <SPtable
+                  data={formattedData}
+              columns={columns}
+              pagination={{ pageSize: 5 }}
+              size="small"
+              bordered
+              scroll={{ y: 180 }}
+              />
+            )}
           </DashboardCardSection>
         </Col>
       </Row>
