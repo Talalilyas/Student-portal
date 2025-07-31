c792da17841b8abb01ccc0d87704ec8b058a0fb8
Talal Ilyas <talalilyas11@gmail.com>
2025-07-31 15:56:19 +0500

    Solved :the hook issue

--- FetchHOOk/Hookfetchdata.js
+++ FetchHOOk/Hookfetchdata.js
@@ -1,39 +1,45 @@
 import { useState, useEffect } from "react";
 
+const reqest = async (
+  url,
+  setData,
+  setLoading,
+  setError,
+  setPostLoading,
+  method = "GET",
+  body = null
+) => {
+  method === "POST" ? setPostLoading(true) : setLoading(true);
+  setError(null);
+
+  try {
+    const response = await fetch(url, {
+      method,
+      headers: body ? { "Content-Type": "application/json" } : undefined,
+      body: body ? JSON.stringify(body) : null,
+    });
+
+    if (!response.ok) throw new Error(`Error: ${response.status}`);
+    const result = await response.json();
+
+    setData(result);
+    return result;
+  } catch (err) {
+    setError(err.message);
+    return null;
+  } finally {
+    method === "POST" ? setPostLoading(false) : setLoading(false);
+  }
+};
+
 const useFetch = (url) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [postLoading, setPostLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const handleRequest = async (method = "GET", body = null) => {
-    method === "POST" ? setPostLoading(true) : setLoading(true);
-    setError(null);
-
-    try {
-      console.log("-----method---", method, "-------url-----", url, "req----body", body);
-      const response = await fetch(url, {
-        method,
-        headers: body ? { "Content-Type": "application/json" } : undefined,
-        body: body ? JSON.stringify(body) : null,
-      });
-
-      if (!response.ok) throw new Error(`Error: ${response.status}`);
-      const result = await response.json();
-
-      setData(result); 
-
-      return result;
-    } catch (err) {
-      setError(err.message);
-      return null;
-    } finally {
-      method === "POST" ? setPostLoading(false) : setLoading(false);
-    }
-  };
-
   useEffect(() => {
-    handleRequest("GET");
+    reqest(url, setData, setLoading, setError, setPostLoading, "GET");
   }, [url]);
 
   return {
@@ -41,8 +47,9 @@ const useFetch = (url) => {
     loading,
     error,
     postLoading,
-    fetchData: () => handleRequest("GET"),
-    postData: (body) => handleRequest("POST", body),
+
+    postData: (body) =>
+      reqest(url, setData, setLoading, setError, setPostLoading, "POST", body),
   };
 };
 
